2404bd4930b550e163c9755cd3a825ba309eee44
CrisTerry <crisdc88@gmail.com>
2020-04-25 15:08:08 -0700

    Add heroku database connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,21 @@
 let mysql = require("mysql")
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    database: "groceries_list",
-    password: "password"
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     database: "groceries_list",
+//     password: "password"
 
+// })
+
+
+var connection = mysql.createConnection({
+    host: "op2hpcwcbxb1t4z9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
+    user: "xms7nfwgy4jbwwgz",
+    database: "wha636dm781fzc3i",
+    password: "vxy93lqz2v2rxsqx"
 })
 
 connection.connect(function(err){
